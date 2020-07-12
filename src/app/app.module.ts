afc4c18d72b021227617eac2760e2fbc78af0c39
ImYusup <yusup,juniadi@outlook.com>
2020-07-12 17:07:04 +0700

    more commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './navigation/header/header.component';
-import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { SomePageComponent } from './some-page/trush-page/some-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoutingModule } from './routing/routing.module';
@@ -14,20 +12,22 @@ import {MatTabsModule, MatSidenavModule,
   MatToolbarModule, MatIconModule, MatButtonModule, 
   MatListModule, MatMenuModule} from '@angular/material';
 import { CommonModule } from '@angular/common';
-import { ContactComponent } from './navigation/contact/contact.component';
 import { DigitalClockComponent } from './home/digital-clock/digital-clock.component';
 import { UtilService } from './home/shared/services/util.service';
-import { ProfileComponent } from './home/profile/profile.component';
 import { DataComponent } from './data/data.component';
 import { NoPageFoundComponent } from './some-page/no-page-found/no-page-found.component';
 import { StudentDetailsComponent } from './data/student-details/student-details.component';
 import { DosenDetailsComponent } from './data/dosen-details/dosen-details.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    SidenavListComponent,AppComponent,HeaderComponent,
-    SomePageComponent, ContactComponent, DigitalClockComponent,
-    ProfileComponent, DataComponent, NoPageFoundComponent, StudentDetailsComponent, DosenDetailsComponent
+    AppComponent,HeaderComponent,
+    SomePageComponent, DigitalClockComponent,
+    DataComponent, NoPageFoundComponent, StudentDetailsComponent, DosenDetailsComponent, FooterComponent, ContactComponent, AboutComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,FormsModule, BrowserAnimationsModule,RoutingModule,
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,22 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { DigitalClockComponent } from '../home/digital-clock/digital-clock.component';
-// import { SomePageComponent } from '../some-page/trush-page/some-page.component'
-import { ContactComponent } from '../navigation/contact/contact.component';
 import { NoPageFoundComponent } from '../some-page/no-page-found/no-page-found.component';
-import { HeaderComponent } from '../navigation/header/header.component';
+import { HeaderComponent } from '../home/header/header.component';
 import { StudentDetailsComponent } from '../data/student-details/student-details.component';
 import { DosenDetailsComponent } from '../data/dosen-details/dosen-details.component';
-// import { ProfileComponent } from '../home/profile/profile.component';
-// import { SidenavListComponent } from '../navigation/sidenav-list/sidenav-list.component';
+import { ContactComponent } from '../contact/contact.component';
+import { AboutComponent } from '../about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: DigitalClockComponent },
-  // { path: 'about', pathMatch: 'prefix', component: SomePageComponent },
   { path: 'data/student', component: StudentDetailsComponent },
   { path: 'data/dosen', component: DosenDetailsComponent },
-  { path: 'contact', component: ContactComponent },
+    { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: NoPageFoundComponent }
 ];
 
